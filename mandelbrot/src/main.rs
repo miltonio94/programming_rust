b4dbeca9a3ab34196d70ed9d7ac5a3e47de1fac6
Milton <milton.depaula@featurespace.co.uk>
2023-04-06 05:13:19 +0100

    WIP
    Emacs keeps freezing on MacOS when programming rust
    I don't want to waste time trying to fix it if I don't have to
    So I'm moving to my linux machine to try to finish this there

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,33 @@
+use num::Complex;
+use std::str::FromStr;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn comples_square_add_loop(c: Complex<u64>) {
+    let mut z: Complex<f64> = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
+
+fn escape_time(c: Complex<u64>, limit: usize) -> Option<usize> {
+    let mut z: Complex<u64> = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    //
+    match s.find(seperator) {
+        None => None,
+        Some(i) => None,
+    }
 }
