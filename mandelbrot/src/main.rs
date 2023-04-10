1c6fd03b9a12843cd525bae84ea3e4f0987a8d4d
Milton <milton94@gmail.com>
2023-04-10 10:39:45 +0100

    Finished the mandel

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -29,16 +29,16 @@ fn main() {
     {
         let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
         crossbeam::scope(|spawner| {
-            for (i, band) in bands.into_iter().enumertate() {
+            for (i, band) in bands.into_iter().enumerate() {
                 let top = rows_per_band * i;
                 let height = band.len() / bounds.0;
-                let bands_bonuds = (bounds.0, height);
+                let bands_bounds = (bounds.0, height);
                 let bands_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
                 let bands_lower_right =
                     pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
                 spawner.spawn(move |_| {
-                    render(band, band_bounds, band_upper_left, band_lower_right);
+                    render(band, bands_bounds, bands_upper_left, bands_lower_right);
                 });
             }
         })
